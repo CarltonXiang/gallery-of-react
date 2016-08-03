917ebaf7ca724f293a1d914d112d80f725cb5ed4
项方伟 <xiangfangwei@xiangfangweideMacBook-Pro.local>
2016-08-03 10:38:02 +0800

    init project structure

---
+++ src/components/Main.js
@@ -0,0 +1,38 @@
+require('normalize.css/normalize.css');
+require('styles/App.scss');
+
+import React from 'react';
+
+//获取图片相关数据
+var imageDatas = require('../data/imagesData.json');
+
+//利用自执行函数，将图片信息转换成图片URL路径信息
+imageDatas = (function genImageURL(imageDatasArr) {
+	for (let i = 0, j = imageDatasArr.length; i < j; i++) {
+		var singleImageData = imageDatasArr[i];
+
+		singleImageData.imageURL = require('../images/' + singleImageData.fileName);
+
+		imageDatasArr[i] = singleImageData;
+	}
+
+	return imageDatasArr;
+})(imageDatas);
+
+class AppComponent extends React.Component {
+  render() {
+    return (
+      <section className="stage">
+      	<section className="img-sec">
+      	</section>
+      	<nav className="controller-nav">
+      	</nav>
+      </section>
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
